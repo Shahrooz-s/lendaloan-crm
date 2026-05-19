8851e261c317d01364c47e541b77aae732f4026b
Shahrooz-s <shahrooz@safanejad.com.au>
2026-05-20 06:18:59 +1000

    Embed OpnForm in Concord web forms settings

--- modules/WebForms/resources/js/components/SettingsWebForms.vue
+++ modules/WebForms/resources/js/components/SettingsWebForms.vue
@@ -1,7 +1,10 @@
 <template>
-  <WebFormsIndex />
+  <div
+    class="h-[calc(100vh-9rem)] min-h-[720px] overflow-hidden rounded-lg border border-neutral-200 bg-white shadow-sm dark:border-neutral-700 dark:bg-neutral-900">
+    <iframe
+      title="OpnForm"
+      src="https://form.lendaloan.com.au"
+      class="h-full w-full border-0"
+      allow="clipboard-read; clipboard-write; fullscreen" />
+  </div>
 </template>
-
-<script setup>
-import WebFormsIndex from '../views/WebFormsIndex.vue'
-</script>
